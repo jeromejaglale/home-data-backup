b2e9191003b8075712cbf000d68b416f4c485f3d
Jerome Jaglale <jerome_jaglale@sfu.ca>
2021-02-10 01:21:51 -0800

    Add comments

--- backup_to_external_hd.sh
+++ backup_to_external_hd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# destination for backups
 externalHDPath='/Volumes/wd_external_2tera_01'
 
 logFolder='/Users/jerome/conf/log'
@@ -8,29 +9,29 @@ logFolder='/Users/jerome/conf/log'
 SECONDS=0
 
 echo "**************************************************************************************"
-echo `date +%Y-%m-%d_%HH%M` "Started backup to $externalHDPath"
+echo `date +%Y-%m-%d_%HH%M` "Starting backups to $externalHDPath.."
 
-# /Volumes/WD_SSD_1TB_01
+# backup /Volumes/WD_SSD_1TB_01
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
 /Users/jerome/bin/rsync.sh /Volumes/WD_SSD_1TB_01/ ${externalHDPath}/backups/WD_SSD_1TB_01 >> $logFilePath 2>&1
 
-# /Users/jerome
+# backup /Users/jerome
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
 /Users/jerome/bin/rsync.sh /Users/jerome/ ${externalHDPath}/backups/HD/jerome >> $logFilePath 2>&1
 
-# /Library
+# backup /Library
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
 /Users/jerome/bin/rsync.sh /Library/ ${externalHDPath}/backups/HD/Library >> $logFilePath 2>&1
 
-# /Applications
+# backup /Applications
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
 /Users/jerome/bin/rsync.sh /Applications/ ${externalHDPath}/backups/HD/Applications >> $logFilePath 2>&1
 
-echo `date +%Y-%m-%d_%HH%M` "Finished"
+echo `date +%Y-%m-%d_%HH%M` "All finished"
 duration=$SECONDS
 echo 'Total time:' "$(($duration / 60)) min" "$(($duration % 60))" 'sec'
 echo "**************************************************************************************"
