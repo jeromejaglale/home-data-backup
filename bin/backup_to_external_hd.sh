c387f78f4ab9cbc80150cdddcca62a84eb6793b6
Jerome Jaglale <jerome_jaglale@sfu.ca>
2021-02-10 02:40:17 -0800

    Add last minute fixes

--- bin/backup_to_external_hd.sh
+++ bin/backup_to_external_hd.sh
@@ -3,13 +3,14 @@
 # destination for backups
 externalHDPath='/Volumes/wd_external_2tera_01'
 
-logFolder="~/log"
+logFolder=~/log
 
 # just to easily calculate script duration: https://stackoverflow.com/questions/8903239/how-to-calculate-time-elapsed-in-bash-script
 SECONDS=0
 
 echo "**************************************************************************************"
 echo `date +%Y-%m-%d_%HH%M` "Starting backups to $externalHDPath.."
+echo
 
 # backup /Volumes/WD_SSD_1TB_01
 startDate=`date +%Y-%m-%d_%HH%M`
@@ -31,6 +32,7 @@ startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
 ~/bin/rsync.sh /Applications/ ${externalHDPath}/backups/HD/Applications >> $logFilePath 2>&1
 
+echo
 echo `date +%Y-%m-%d_%HH%M` "All finished"
 duration=$SECONDS
 echo 'Total time:' "$(($duration / 60)) min" "$(($duration % 60))" 'sec'
