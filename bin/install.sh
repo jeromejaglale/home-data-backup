bc82399a9108795a0df3c55641932f44d35e78ad
Jerome Jaglale <jerome_jaglale@sfu.ca>
2021-02-10 02:08:24 -0800

    put rsync_exclude.txt in bin folder

--- bin/install.sh
+++ bin/install.sh
@@ -2,19 +2,15 @@
 
 SCRIPT_DIR_FULL="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# install/replace backup scripts in ~/bin
+# install/replace backup files in ~/bin
 mkdir -p ~/bin
 rm -f ~/bin/rsync.sh ~/bin/backup_to_external_hd.sh
 ln -s ${SCRIPT_DIR_FULL}/rsync.sh ~/bin/rsync.sh
 ln -s ${SCRIPT_DIR_FULL}/backup_to_external_hd.sh ~/bin/backup_to_external_hd.sh
-
-# install/replace rsync exclude file list in ~/conf
-mkdir -p ~/conf
-rm -f ~/conf/rsync_exclude.txt
-ln -s ${SCRIPT_DIR_FULL}/../conf/rsync_exclude.txt ~/conf/rsync_exclude.txt
+ln -s ${SCRIPT_DIR_FULL}/rsync_exclude.txt ~/bin/rsync_exclude.txt
 
 # create log folder
-mkdir -p ~/conf/log
+mkdir -p ~/log
 
 # install/replace crontab
 crontab ${SCRIPT_DIR_FULL}/../conf/crontab.txt
