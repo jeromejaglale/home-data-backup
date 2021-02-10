6f80547ff36ed27baf9c7f6cf018409457af3bf0
Jerome Jaglale <jerome_jaglale@sfu.ca>
2021-02-10 02:28:38 -0800

    Add last minute fixes

--- bin/backup_to_external_hd.sh
+++ bin/backup_to_external_hd.sh
@@ -3,7 +3,7 @@
 # destination for backups
 externalHDPath='/Volumes/wd_external_2tera_01'
 
-logFolder='/Users/jerome/conf/log'
+logFolder="~/log"
 
 # just to easily calculate script duration: https://stackoverflow.com/questions/8903239/how-to-calculate-time-elapsed-in-bash-script
 SECONDS=0
@@ -14,22 +14,22 @@ echo `date +%Y-%m-%d_%HH%M` "Starting backups to $externalHDPath.."
 # backup /Volumes/WD_SSD_1TB_01
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
-/Users/jerome/bin/rsync.sh /Volumes/WD_SSD_1TB_01/ ${externalHDPath}/backups/WD_SSD_1TB_01 >> $logFilePath 2>&1
+~/bin/rsync.sh /Volumes/WD_SSD_1TB_01/ ${externalHDPath}/backups/WD_SSD_1TB_01 >> $logFilePath 2>&1
 
 # backup /Users/jerome
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
-/Users/jerome/bin/rsync.sh /Users/jerome/ ${externalHDPath}/backups/HD/jerome >> $logFilePath 2>&1
+~/bin/rsync.sh /Users/jerome/ ${externalHDPath}/backups/HD/jerome >> $logFilePath 2>&1
 
 # backup /Library
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
-/Users/jerome/bin/rsync.sh /Library/ ${externalHDPath}/backups/HD/Library >> $logFilePath 2>&1
+~/bin/rsync.sh /Library/ ${externalHDPath}/backups/HD/Library >> $logFilePath 2>&1
 
 # backup /Applications
 startDate=`date +%Y-%m-%d_%HH%M`
 logFilePath=${logFolder}/backup_${startDate}.log
-/Users/jerome/bin/rsync.sh /Applications/ ${externalHDPath}/backups/HD/Applications >> $logFilePath 2>&1
+~/bin/rsync.sh /Applications/ ${externalHDPath}/backups/HD/Applications >> $logFilePath 2>&1
 
 echo `date +%Y-%m-%d_%HH%M` "All finished"
 duration=$SECONDS
