04eb60aad367d701bcc71be37d28f7a9ce72c37f
Jerome Jaglale <jerome_jaglale@sfu.ca>
2021-02-10 01:38:30 -0800

    Add install script

---
+++ bin/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SCRIPT_DIR_FULL="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# install/replace backup scripts in ~/bin
+mkdir -p ~/bin
+rm -f ~/bin/rsync.sh ~/bin/backup_to_external_hd.sh
+ln -s ${SCRIPT_DIR_FULL}/rsync.sh ~/bin/rsync.sh
+ln -s ${SCRIPT_DIR_FULL}/backup_to_external_hd.sh ~/bin/backup_to_external_hd.sh
+
+# install/replace rsync exclude file list in ~/conf
+mkdir -p ~/conf
+rm -f ~/conf/rsync_exclude.txt
+ln -s ${SCRIPT_DIR_FULL}/../conf/rsync_exclude.txt ~/conf/rsync_exclude.txt
+
+# install/replace crontab
+crontab ${SCRIPT_DIR_FULL}/../conf/crontab.txt
